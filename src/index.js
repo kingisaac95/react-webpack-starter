5ec760ae1c54e5178f8b2d387fd36699303900e1
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-21 16:14:22 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Root = () => <div>Hello world! From React :)</div>
+
+ReactDOM.render(<Root />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
