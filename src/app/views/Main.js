2cc02c44cc930ac68616835162c3b756c769ab37
Orjiewuru Kingdom <kingdom.orjiewuru@andela.com>
2018-08-21 20:59:19 +0100

    update styling

--- src/app/views/Main.js
+++ src/app/views/Main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Content from '../components/Content';
 
-import '../../favicons/favicons';
-
 const Main = () => (
   <div>
     <Header />
